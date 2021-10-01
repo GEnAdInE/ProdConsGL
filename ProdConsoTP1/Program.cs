98e3e21495b07ab4a0c67a091fdfd6f74a9e0e52
Robin <one.robin@outlook.fr>
2021-10-01 15:42:10 +0200

    first commit

---
+++ ProdConsoTP1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdConsoTP1
+{
+    public class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Stock MyStock;
+            string sMyInput = string.Empty;
+            int iValueToAdd = 0;
+            int iCurrentValue = 0;
+
+            Console.WriteLine("Projet prod/conso\n");
+
+            MyStock = new Stock(10,2,8);
+
+            while(true)
+            {
+                Console.WriteLine(MyStock.GetCurrent()+"\n");
+
+                 sMyInput = Console.ReadLine();
+                
+                if(sMyInput.StartsWith("PUSH"))
+                {
+                    int.TryParse(sMyInput.Remove(0, 5),out iValueToAdd);
+                    MyStock.Push(iValueToAdd);
+                }
+
+
+            }
+
+
+        }
+
+
+       
+    }
+}
---
+++ ProdConsoTP1/Stock.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdConsoTP1
+{
+    public class Stock
+    {
+        public delegate void CustomEvent(int iValue);
+
+        public CustomEvent OnMaxLimit;
+        public CustomEvent OnminLimit;
+
+
+        private Stack<int> myStack;
+        private int iMaxAlert;
+        private int iMinAlert;
+        public Stock(int _size,int _min, int _max)
+        {
+            myStack = new Stack<int>(_size);
+            iMaxAlert = _max;
+            iMinAlert = _min;
+        }
+
+        public void Push(int iProduct)
+        {
+            myStack.Push(iProduct);
+            if (myStack.Count >= iMaxAlert)
+                OnMaxLimit(myStack.Count);
+        }
+
+        public int Pop()
+        {
+            int iRes = myStack.Pop();
+            if (myStack.Count <= iMinAlert)
+                OnminLimit(myStack.Count);
+            return iRes;
+        }
+
+        public void Clear()
+        {
+            myStack.Clear();
+        }
+
+        public int GetCurrent()
+        {
+           return myStack.Peek();
+        }
+    }
+}
